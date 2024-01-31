1e7be7fb7509cc549b771b379cb2d60f4e65aee2
sigmamonster69 <157638838+sigmamonster69@users.noreply.github.com>
2024-01-31 10:11:50 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
-
 int main()
-{ int x; std::cout << "Enter a number: ";
-
-
-std::cin >> x;
-std::cout << "Your number is: " << x * 10 <<
-std::endl; return 0;
+{
+    int x;
+    std::cout << "Enter a number: ";
+    std::cin >> x;
+    std::cout << "Your number multiplied by 10 is: " << x * 10 << std::endl;
+    return 0;
 }
